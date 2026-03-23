73b49d4385e87e23be75e9f58d04b7e4ff4434ac
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-23 14:42:44 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-
 import { TasksProvider } from "./contexts/TasksProvider";
 import { TaskBoard } from "./components/TaskBoard";
-// import { Modal } from 'bootstrap'
+import { CreateTaskForm } from "./components/CreateTaskForm";
+// import { Modal } from "bootstrap";
 
 function App() {
-  // const modal = new Modal("#createTaskModal")
+  // const modal = new Modal("#createTaskModal");
 
   return (
-    <TasksProvider
-    >
+    <TasksProvider>
       <div className="container-fluid">
         <div className="d-flex align-items-center gap-4">
           <h1 className="position-sticky top-0">React Kanban</h1>
+
           <button
             type="button"
             className="btn btn-primary"
-            // onClick={() => modal.toggle()}
+            data-bs-toggle="modal"
+            data-bs-target="#createTaskModal"
           >
             Nova tarefa
           </button>
@@ -24,41 +25,41 @@ function App() {
             className="modal fade"
             id="createTaskModal"
             tabIndex={-1}
-            aria-labelledby="createTaskModal"
-            aria-hidden
+            aria-labelledby="createTaskModalLabel"
+            aria-hidden="true"
           >
             <div className="modal-dialog">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h2 className="modal-title">Nova tarefa</h2>
+                  <h2 className="modal-title" id="createTaskModalLabel">
+                    Nova tarefa
+                  </h2>
+
                   <button
                     type="button"
                     className="btn-close"
+                    data-bs-dismiss="modal"
                     aria-label="Close"
-                    // onClick={() => modal.toggle()}
                   ></button>
                 </div>
+
                 <div className="modal-body">
-                
+                  <CreateTaskForm />
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="">
-          <div>
-         
-          </div>
-          <div className="">
+        <div>
+          <div className="mt-4">
             <h2 className="mb-3">Quadro de tarefas</h2>
             <TaskBoard />
           </div>
         </div>
-        {/* <UpdateTaskModal /> */}
       </div>
     </TasksProvider>
-  )
+  );
 }
 
-export default App
+export default App;
