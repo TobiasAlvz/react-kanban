6f61f2f8f921f58ce733aabf051fbf4d0d3718b2
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-23 16:11:47 -0300

    Update TasksContext.ts

--- src/contexts/TasksContext.ts
+++ src/contexts/TasksContext.ts
@@ -3,12 +3,15 @@ import type { Task } from "../entities/Task";
 
 export interface TasksContextData {
   tasks: Task[];
+  loading: boolean;
+  error: string | null;
   createTask: (attributes: Omit<Task, "id">) => Promise<Task>;
   updateTask: (
-    id: number,
-    attributes: Partial<Omit<Task, "id">>,
+    id: string | number,
+    attributes: Partial<Omit<Task, "id">>
   ) => Promise<void>;
-  deleteTask: (id: number) => Promise<void>;
+  deleteTask: (id: string | number) => Promise<void>;
+  loadTasks: () => Promise<void>;
 }
 
 export const TasksContext = createContext({} as TasksContextData);
