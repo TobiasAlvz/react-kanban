1cadee7ceac52fe1b24fb7559f7c32970b83f86b
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-23 14:42:46 -0300

    Update CreateTaskForm.tsx

--- src/components/CreateTaskForm.tsx
+++ src/components/CreateTaskForm.tsx
@@ -1,35 +1,145 @@
-import { useContext } from "react"
-import { TasksContext } from "../contexts/TasksContext"
-import { TaskCard } from "./TaskCard"
+import { useContext, type FormEventHandler } from "react";
+import { TasksContext } from "../contexts/TasksContext";
+import { z } from "zod";
 
-export const TaskBoard: React.FC = () => {
-  const { tasks } = useContext(TasksContext)
+const CreateTaskSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  status: z.enum(["todo", "doing", "done"]),
+  priority: z.enum(["low", "medium", "high"]),
+});
+
+export const CreateTaskForm: React.FC = () => {
+  const { createTask } = useContext(TasksContext);
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (ev) => {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.currentTarget);
+
+    const taskData = CreateTaskSchema.parse({
+      title: formData.get("title"),
+      description: formData.get("description"),
+      status: formData.get("status"),
+      priority: formData.get("priority"),
+    });
+
+    await createTask(taskData);
+
+    ev.currentTarget.reset();
+  };
 
   return (
-    <section className="row">
-      <div className="col-12 col-lg-4">
-        {tasks
-          .filter((task) => task.status === "todo")
-          .map((task) => (
-            <TaskCard key={task.id} task={task} />
-          ))}
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="title">Título</label>
+        <input
+          className="form-control"
+          type="text"
+          name="title"
+          id="title"
+          autoFocus
+        />
       </div>
 
-      <div className="col-12 col-lg-4">
-        {tasks
-          .filter((task) => task.status === "doing")
-          .map((task) => (
-            <TaskCard key={task.id} task={task} />
-          ))}
+      <div className="mb-3">
+        <label htmlFor="description">Descrição</label>
+        <textarea
+          className="form-control"
+          name="description"
+          id="description"
+        ></textarea>
       </div>
 
-      <div className="col-12 col-lg-4">
-        {tasks
-          .filter((task) => task.status === "done")
-          .map((task) => (
-            <TaskCard key={task.id} task={task} />
-          ))}
+      <div className="mb-3">
+        <div>Status</div>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="status"
+            id="todo"
+            value="todo"
+          />
+          <label className="form-check-label" htmlFor="todo">
+            Para Fazer
+          </label>
+        </div>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="status"
+            id="doing"
+            value="doing"
+          />
+          <label className="form-check-label" htmlFor="doing">
+            Em Progresso
+          </label>
+        </div>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="status"
+            id="done"
+            value="done"
+          />
+          <label className="form-check-label" htmlFor="done">
+            Completada
+          </label>
+        </div>
       </div>
-    </section>
-  )
-}
+
+      <div className="mb-3">
+        <div>Prioridade</div>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="priority"
+            id="low"
+            value="low"
+          />
+          <label className="form-check-label" htmlFor="low">
+            Baixa
+          </label>
+        </div>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="priority"
+            id="medium"
+            value="medium"
+          />
+          <label className="form-check-label" htmlFor="medium">
+            Média
+          </label>
+        </div>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="priority"
+            id="high"
+            value="high"
+          />
+          <label className="form-check-label" htmlFor="high">
+            Alta
+          </label>
+        </div>
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Criar Tarefa
+      </button>
+    </form>
+  );
+};
